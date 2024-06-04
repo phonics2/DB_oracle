3ed0345cc8bfd5bec99a0dbe0daa08a6e71c12b5
phonics2 <ha4514@naver.com>
2024-06-04 17:31:37 +0900

    join 춘대학교실습

--- SQL_개념정리/06_JOIN.sql
+++ SQL_개념정리/06_JOIN.sql
@@ -160,6 +160,77 @@ E.EMP_ID, E.EMP_NAME, E.JOB_CODE, J.JOB_NAME
 202	        노옹철	     J2	        부사장
 */
 
+/***** OUTER JOIN 외부조인 *****/
+-- 두 테이블의 지정하는 컬럼값이 일치하지 않는 행도
+-- 조인에 포함 시킴
+
+-- OUTER JOIN 과 비교할 INNER JOIN 쿼리문
+
+SELECT EMP_NAME, DEPT_TITLE
+FROM EMPLOYEE
+/*INNER*/JOIN DEPARTMENT ON (DEPT_CODE = DEPT_ID);
+
+-- 1) LEFT [OUTER] JOIN
+-- 합치기 사용한 두 테이블 중에서 왼쪽 편에 작성된 테이블의
+-- 컬럼수를 기준으로 JOIN
+
+-- ANSI 표준
+SELECT EMP_NAME, DEPT_TITLE
+FROM EMPLOYEE LEFT JOIN DEPARTMENT ON (DEPT_CODE = DEPT_ID);
+-- JOIN 구문 기준으로 왼쪽에 작성된 테이블의
+-- 모든 행이 최종 결과(RESULT SET)에 포함되도록 하는 조인
+
+--- ORACLE 표준
+SELECT EMP_NAME, DEPT_TITLE
+FROM EMPLOYEE, DEPARTMENT
+WHERE DEPT_CODE = DEPT_ID(+);
+-- DEPT_CODE와 DEPT_ID 값이 일치하지 않아도
+-- 왼쪽에 있는 컬럼수에 맞게 표시를 넣어라(NULL 값이라도 넣어라)
+
+
+
+
+-- 2) RIGHT [OUTER] JOIN
+-- 합치기 사용한 두 테이블 중에서 오른쪽 편에 작성된 테이블의
+-- 컬럼수를 기준으로 JOIN
+
+-- ANSI 표준
+SELECT EMP_NAME, DEPT_TITLE
+FROM EMPLOYEE RIGHT JOIN DEPARTMENT ON (DEPT_CODE = DEPT_ID);
+-- JOIN 구문 기준으로 오른쪽에 작성된 테이블의
+-- 모든 행이 최종 결과(RESULT SET)에 포함되도록 하는 조인
+
+--- ORACLE 표준
+SELECT EMP_NAME, DEPT_TITLE
+FROM EMPLOYEE, DEPARTMENT
+WHERE DEPT_CODE(+) = DEPT_ID;
+-- DEPT_CODE와 DEPT_ID 값이 일치하지 않아도
+-- 오른쪽에 있는 컬럼수에 맞게 표시를 넣어라(NULL 값이라도 넣어라)
+
+
+
+
+-- 2) FULL [OUTER] JOIN
+-- 합치기 사용한 두 테이블 이 가진 모든 행을 결과에 포함
+--> 오라클에서는 FULL OUTER JOIN 구문 사용 불가
+
+-- ANSI 표준
+SELECT EMP_NAME, DEPT_TITLE
+FROM EMPLOYEE FULL JOIN DEPARTMENT ON (DEPT_CODE = DEPT_ID);
+-- JOIN 구문 기준으로 양쪽에 작성된 테이블의
+-- 모든 행이 최종 결과(RESULT SET)에 포함되도록 하는 조인
+
+--- ORACLE 표준
+SELECT EMP_NAME, DEPT_TITLE
+FROM EMPLOYEE, DEPARTMENT
+WHERE DEPT_CODE(+) = DEPT_ID(+);
+/*
+ORA-01468: a predicate may reference only one outer-joined table
+01468. 00000 -  "a predicate may reference only one outer-joined table"
+*Cause:    
+*Action:
+226행, 20열에서 오류 발생
+*/
 
 
 
